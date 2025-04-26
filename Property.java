506de94af88ced567ddecd5d4ae40633d481897e
Phạm Trần Gia Hưng <giahung1510200519@gmail.com>
2025-04-26 09:36:49 +0700

    soemthing

---
+++ Property.java
@@ -0,0 +1,11 @@
+public class Property {
+    int PropertyId;
+    String Description;
+    int PropertyOwnerId;
+    public Property(int propertyId, String description, int propertyOwnerId) {
+        this.PropertyId = propertyId;
+        this.Description = description;
+        this.PropertyOwnerId = propertyOwnerId;
+    }
+    
+}
