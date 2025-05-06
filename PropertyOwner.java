548615cf904059bc5ac3f778ac8493fcbcccd486
Phạm Việt Anh <155903449+eiuphamvietanh@users.noreply.github.com>
2025-05-06 16:06:01 +0700

    Viet Anh has done PropertyOwner

--- PropertyOwner.java
+++ PropertyOwner.java
@@ -2,28 +2,48 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class PropertyOwner {
-
+    
     private User user;
-    private List<Property> listOfProperties = new ArrayList<>();
+    private List<Property> listOfProperties;
 
-    public PropertyOwner(User user, List<Property> listOfProperties) {
+    public PropertyOwner(User user) {
         this.user = user;
-        this.listOfProperties = listOfProperties;
+        this.listOfProperties = new ArrayList<>();
     }
 
+    
     public void addProperty(Property property) {
-        listOfProperties.add(property);
+        if (!listOfProperties.contains(property)) {
+            listOfProperties.add(property);
+            System.out.println("Property added successfully.");
+        } else {
+            System.out.println("Property already exists.");
+        }
     }
 
-    public void removeProperty(Property property) {
-        listOfProperties.remove(property);
+    
+    public void removeProperty(String propertyID) {
+        Property toRemove = null;
+        for (Property p : listOfProperties) {
+            if (p.getPropertyID().equals(propertyID)) {
+                toRemove = p;
+                break;
+            }
+        }
+        if (toRemove != null) {
+            listOfProperties.remove(toRemove);
+            System.out.println("Property removed successfully.");
+        } else {
+            System.out.println("Property not found.");
+        }
     }
 
+  
     public User getUser() {
         return user;
     }
 
-    public void setUser(User user) {
-        this.user = user;
+    public List<Property> getListOfProperties() {
+        return listOfProperties;
     }
 }
